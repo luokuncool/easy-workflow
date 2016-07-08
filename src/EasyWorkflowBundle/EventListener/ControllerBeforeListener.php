b039275b7588e0984f974cf790fd0c44cfba0faa
quentin.luo <1131915347@qq.com>
2016-07-08 18:15:26 +0800

    请假发起界面

--- src/EasyWorkflowBundle/EventListener/ControllerBeforeListener.php
+++ src/EasyWorkflowBundle/EventListener/ControllerBeforeListener.php
@@ -9,7 +9,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
 
-class FlowListener
+class ControllerBeforeListener
 {
     private $container;
 
@@ -27,18 +27,12 @@ class FlowListener
         }
 
         if ($token = $this->container->get('security.token_storage')->getToken()) {
-            $activeUser = $this->container->get('security.token_storage')->getToken()->getUser();
+            $activeUser = $token->getUser();
             $this->container->get('twig')->addGlobal('activeUser', $activeUser);
         }
 
         if ($controller[0] instanceof FlowInterface) {
-            if ($event->getRequest()->isXmlHttpRequest()) {
-                $event->setController(
-                    function () {
-                        return JsonResponse::create($_SERVER);
-                    }
-                );
-            }
+
         }
     }
 }
